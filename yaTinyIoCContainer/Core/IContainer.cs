661b99fc50c2f91f5a3ab99b13afaa6ebfe681fe
rahulsinghhfs <rahuls@headfitted.com>
2016-02-01 11:01:06 +0530

    Some code re-factoring has been done.

--- yaTinyIoCContainer/Core/IContainer.cs
+++ yaTinyIoCContainer/Core/IContainer.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace yaTinyIoCContainer.Core
+﻿namespace yaTinyIoCContainer.Core
 {
     public interface IContainer
     {
